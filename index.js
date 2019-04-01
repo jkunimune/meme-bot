3a56b4d943cd06336c67116f9663c27d4a1b2780
Justin Kunimune <justinkunimune@gmail.com>
2019-04-01 12:20:43 -0400

    More memes!
    I'm sorry. I'm so sorry.

--- index.js
+++ index.js
@@ -10,15 +10,33 @@ client.on('ready', () => {
 client.on('message', message => {
 	content = message.content.toLowerCase();
 	
-	if (isReady && (content.includes('bean') || content.includes('spill') || content.includes('eat') || content.includes('life is strange'))) {
-		isReady = false;
-		var voiceChannel = message.member.voiceChannel;
-		voiceChannel.join().then(connection => {
-			const dispatcher = connection.playFile('./res/beans.mp3');
-			dispatcher.on("end", end => {voiceChannel.leave()});
-		}).catch(err => console.log(err));
-		isReady = true;
-	}
+	songs = [
+		['beans', ['bean','spill','eat','fucking insane','life is strange']],
+		['mii', ['mii','doot','yumbo','music']],
+		['iim', ['justin', 'jason', 'nosaj', 'nitsuj', 'natsuji', 'galactic ketchup']],
+		['yahaha', ['korok', 'yahaha', 'seed', 'ha']],
+		['heartbreak', ['sad', 'die', 'broken', 'determination']],
+		['cat', ['feed', 'dying', 'charger', 'hungry']],
+		['dog', ['dog', 'cute', 'annoying', 'toby fox']],
+	];
+	
+	songs.forEach(songInfo => {
+		const song = songInfo[0];
+		const triggers = songInfo[1];
+		triggers.forEach(trigger => {
+			if (isReady && content.includes(trigger)) {
+				isReady = false;
+				var voiceChannel = message.member.voiceChannel;
+				voiceChannel.join().then(connection => {
+					const dispatcher = connection.playFile('./res/'+song+'.mp3');
+					dispatcher.on('end', end => {
+						voiceChannel.leave();
+						isReady = true;
+					});
+				}).catch(err => console.log(err));
+			}
+		});
+	});
 });
 
 client.login(auth.token);
