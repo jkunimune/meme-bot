29d79bbe5c81b13e5359da0c88b3f2f68b6cd210
root <root@vultr.guest>
2022-07-04 14:29:16 +0000

    maintainance
    he wasn't working as I suspected, so I fixd him.

--- yumbo.py
+++ yumbo.py
@@ -5,21 +5,27 @@ import re
 from dotenv import load_dotenv
 import discord
 
+PORTMANTO_BAR = 3
 SONGS = {
-	'beans':      ["bean", "spill", "eating", "fuckinginsane", "lifeisstrange"],
+	'beans':      ["bean", "spill", "eating", "fucking insane", "life is strange"],
 	'mii':        ["mii", "doot", "yumbo", "music"],
-	'iim':        ["justin", "jason", "nosaj", "nitsuj", "natsuji", "galacticketchup", "kate"],
-	'yahaha':     ["korok", "yahaha", "seed", "lol", "hahaha", "heh", "liftrock"],
+	'iim':        ["justin", "jason", "nosaj", "nitsuj", "natsuji", "galactic ketchup", "kate"],
+	'yahaha':     ["korok", "yahaha", "seed", "lol", "hahaha", "heh", "lift rock"],
 	'heartbreak': ["sad", "death", "broken", "determination", "rip"],
 	'cat':        ["feed", "dying", "charger", "hungry", "cat"],
-	'dog':        ["dog", "cute", "annoying", "tobyfox", "woof"],
+	'dog':        ["dog", "cute", "annoying", "toby fox", "woof"],
 	'noting':     ["noting"],
 	'laugh':      ["muaha", "flowey", "kill"],
 	'aisunao':    ["japan", "naoshima", "restaurant", "aisunao"],
-	'hello':      ["hello", "hearme", "hearyou", "youstillthere", "anyonethere"],
-	'photograph': ["photograph", "lookatthis", "itmakesmelaugh"],
-	'objection':  ["objection", "holdit", "notsofast", "stoprightthere", "lies"],
+	'hello':      ["hello", "hear me", "hear you", "you still there", "anyone there"],
+	'photograph': ["photograph", "look at this", "it makes me laugh"],
+	'objection':  ["objection", "hold it", "not so fast", "stop right there", "lies"],
 	'why':        ["why"],
+}
+REACTIONS = {
+	'r\.?i\.?p': '🇫',
+	'long\s?long': '🎷',
+	'loo+ng': '🎷',
 }
 
 load_dotenv()
@@ -45,7 +51,7 @@ def lev_dist(first, twoth, memo={}):
 
 @client.event
 async def on_ready():
-	print("redi!")
+	print("junbe!")
 
 @client.event
 async def on_message(message):
@@ -55,18 +61,18 @@ async def on_message(message):
 		return
 
 	if client.user in message.mentions: # respond to direct messages
-		print("le me!")
+		print("go ga mi!")
 		await message.channel.send("It me.")
 
-	content = message.content.lower()
+	content = message.content
 
-	words = content.split() # make combinacion puns
+	words = content.lower().split() # make combinacion puns
 	for i in range(len(words) - 1):
 		first, twoth = words[i], words[i+1]
 		if re.fullmatch(r'[a-z]+', first) and re.fullmatch(r'[a-z]+', twoth):
-			for j in range(1, len(first) - 4):
-				if first[j:j+4] in twoth[:-1]:
-					portmanto = first[:j] + twoth[twoth.index(first[j:j+4]):]
+			for j in range(1, len(first) - PORTMANTO_BAR):
+				if first[j:j + PORTMANTO_BAR] in twoth[:-1]:
+					portmanto = first[:j] + twoth[twoth.index(first[j:j + PORTMANTO_BAR]):]
 					if lev_dist(first, portmanto) > 1 and lev_dist(twoth, portmanto) > 0:
 						print(f"epick pun detected: {first} + {twoth} = {portmanto}")
 						await message.channel.send(f"{portmanto.capitalize()}, if you will.")
@@ -85,11 +91,9 @@ async def on_message(message):
 			lines = [line.strip() for line in f]
 			await message.channel.send(random.choice(lines))
 
-	content = re.sub(r'[ .,;:!?\-_/\'"“”]', '', content)
-
 	for song, triggers in SONGS.items(): # if someone says "beans", play the beans song
 		for trigger in triggers:
-			if client.ready_to_play and trigger in content:
+			if client.ready_to_play and trigger in content.lower():
 				if message.author.voice is not None and message.author.voice.channel is not None:
 					voice_channel = message.author.voice.channel
 					if voice_channel is not None:
@@ -115,22 +119,24 @@ async def on_message(message):
 					await message.channel.send(line) # and send this one
 				break
 			elif len(line) > 0:
-				bare_line = re.sub(r'[.,;:!?-_\'’"“”* ]', '', line.lower())
-				if content == bare_line or (len(bare_line) >= 7 and content.endswith(bare_line)): # if this line matches normally
-					matched = True # mark it
-					groups = []
-					print('sensa retrologe da "{}"'.format(line))
-				elif line.startswith('/') and line.endswith('/')
-					bare_line = re.sub(r'[.,;:!?-_\'’“”*]', '', line[1:-1])
-					match = re.search(bare_line, content, re.IGNORECASE) # if this line matches via regex
+				if line.startswith('/') and line.endswith('/'):
+					bare_content = re.sub(r'[.,;:!?‽\-_\/\'’"“”*>]', '', content)
+					bare_line = line[1:-1]
+					match = re.search(bare_line, bare_content, re.IGNORECASE) # if this line matches via regex
 					if match:
 						matched = True
-						groups = line_match.groups() # mark it and save the groops
+						groups = match.groups() # mark it and save the groops
+						print('sensa retrologe da "{}"'.format(line))
+				else:
+					bare_content = re.sub(r'[.,;:!?\-_\/\'’"“”*> ]', '', content.lower())
+					bare_line = re.sub(r'[.,;:!?‽\-_\/\'’"“”*> ]', '', line.lower())
+					if bare_content == bare_line or (len(bare_line) >= 7 and bare_content.endswith(bare_line)): # if this line matches normally
+						matched = True # mark it
+						groups = []
 						print('sensa retrologe da "{}"'.format(line))
 
-	if content == 'rip':
-		await message.add_reaction('🇫')
-	if 'long long' in content or re.match(r'loo+ng', content):
-		await message.add_reaction('🎷')
+	for key, reaction in REACTIONS.items():
+		if re.search(key, content, re.IGNORECASE):
+			await message.add_reaction(reaction)
 
 client.run(os.getenv('TOKEN'))
