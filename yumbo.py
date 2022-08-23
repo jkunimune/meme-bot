711712b1fec0d34b83253bbf5f78f4890f527145
Justin Kunimune <justinkunimune@gmail.com>
2022-08-23 13:29:43 -0400

    it's like poetry it rhymes
    I made it not repeat lines from the $line command, and also added restrictions on the portmanto jokes, and also caut an error.

--- yumbo.py
+++ yumbo.py
@@ -10,7 +10,7 @@ SONGS = {
 	'beans':      ["bean", "spill", "eating", "fucking insane", "life is strange"],
 	'mii':        ["mii", "doot", "yumbo", "music"],
 	'iim':        ["justin", "jason", "nosaj", "nitsuj", "natsuji", "galactic ketchup", "kate"],
-	'yahaha':     ["korok", "yahaha", "seed", "lol", "hahaha", "heh", "lift rock"],
+	'yahaha':     ["korok", "yahaha", "seed", "lol", "heh", "lift rock"],
 	'heartbreak': ["sad", "death", "broken", "determination", "rip"],
 	'cat':        ["feed", "dying", "charger", "hungry", "cat"],
 	'dog':        ["dog", "cute", "annoying", "toby fox", "woof"],
@@ -23,15 +23,16 @@ SONGS = {
 	'why':        ["why"],
 }
 REACTIONS = {
-	'r\.?i\.?p': '🇫',
-	'long\s?long': '🎷',
-	'loo+ng': '🎷',
+	r'\br\.?i\.?p\b': '🇫',
+	r'long\s?long': '🎷',
+	r'loo+ng': '🎷',
 }
 
 load_dotenv()
 
 client = discord.Client()
 client.ready_to_play = True
+client.blacklist = []
 
 def lev_dist(first, twoth, memo={}):
 	if (first, twoth) not in memo:
@@ -48,6 +49,12 @@ def lev_dist(first, twoth, memo={}):
 				lev_dist(first[1:], twoth[1:]))
 		memo[(first, twoth)] = dist
 	return memo[(first, twoth)]
+
+def no_double_consonants(s):
+	for k in range(0, len(s) - 1):
+		if s[k] == s[k + 1] and s[k] not in "aeiouy":
+			return False
+	return True
 
 @client.event
 async def on_ready():
@@ -67,46 +74,40 @@ async def on_message(message):
 	content = message.content
 
 	words = content.lower().split() # make combinacion puns
-	for i in range(len(words) - 1):
+	for i in range(max(0, len(words) - 16), len(words) - 1): # if it's near the end of the message
 		first, twoth = words[i], words[i+1]
-		if re.fullmatch(r'[a-z]+', first) and re.fullmatch(r'[a-z]+', twoth):
+		if re.fullmatch(r'[a-z]+', first) and re.fullmatch(r'[a-z]+', twoth): # and both components are purely alphabetical
 			for j in range(1, len(first) - PORTMANTO_BAR):
-				if first[j:j + PORTMANTO_BAR] in twoth[:-1]:
+				if first[j:j + PORTMANTO_BAR] in twoth: # and you sense a portman
 					portmanto = first[:j] + twoth[twoth.index(first[j:j + PORTMANTO_BAR]):]
-					if lev_dist(first, portmanto) > 1 and lev_dist(twoth, portmanto) > 0:
-						print(f"epick pun detected: {first} + {twoth} = {portmanto}")
-						await message.channel.send(f"{portmanto.capitalize()}, if you will.")
-						break
+					if no_double_consonants(first[j:j + PORTMANTO_BAR]): # and there are no double letters in the shared part
+						if portmanto not in first and portmanto not in twoth and \
+								lev_dist(first, portmanto) > 1 and lev_dist(twoth, portmanto) > 0: # and it's sufficiently distant from its components
+							print(f"epick pun detected: {first} + {twoth} = {portmanto}")
+							if random.random() < 1/3:
+								await message.channel.send(f"{portmanto.capitalize()}, if you will.")
+							break
+						else:
+							print(f"passing on {first} + {twoth} = {portmanto} because it's too close to its components")
 					else:
-						print(f"passing on {portmanto} because it's too close to its components")
+						print(f"passing on {first} + {twoth} = {portmanto} because it doesn't *feel* like they share {PORTMANTO_BAR} zi")
 
 	if content == '$roll' or content == '$2d6': # if someone says "/roll" or something of the sort, return a random number [1, 37)
 		await message.channel.send("(sound of rolling dies)")
 		with open('./res/faces.txt', 'r') as f:
 			faces = [line.strip() for line in f]
-			await message.channel.send(random.choice(faces))
+		await message.channel.send(random.choice(faces))
 
 	elif content == '$line': # if someone asks for a line, give them one
 		with open('./res/lines.txt', 'r') as f:
-			lines = [line.strip() for line in f]
-			await message.channel.send(random.choice(lines))
-
-	for song, triggers in SONGS.items(): # if someone says "beans", play the beans song
-		for trigger in triggers:
-			if client.ready_to_play and trigger in content.lower():
-				if message.author.voice is not None and message.author.voice.channel is not None:
-					voice_channel = message.author.voice.channel
-					if voice_channel is not None:
-						client.ready_to_play = False
-						print("zayo sonda {}.mp3".format(song))
-						voice_client = await voice_channel.connect()
-						voice_client.play(discord.FFmpegPCMAudio('./res/{}.mp3'.format(song)))
-						while voice_client.is_playing():
-							await asyncio.sleep(1)
-						await voice_client.disconnect()
-						print("lewo sonda {}.mp3".format(song))
-						client.ready_to_play = True
-						break
+			lines = {line.strip() for line in f}
+		for line in client.blacklist:
+			lines.remove(line) # make sure it's not one that's come up recently
+		choice = random.choice(list(lines))
+		client.blacklist.append(choice)
+		if len(client.blacklist) > 18:
+			client.blacklist = client.blacklist[1:]
+		await message.channel.send(f"||{choice}||")
 
 	with open('./res/scripts.txt', 'r') as f: # if someone says "understand", tell them about how heir soul will transform this world
 		matched, groups = False, []
@@ -116,7 +117,10 @@ async def on_message(message):
 				if len(line) > 0:
 					for i, group in enumerate(groups): # fill in any groups from that match
 						line = line.replace(f'${i + 1}', group)
-					await message.channel.send(line) # and send this one
+					try:
+						await message.channel.send(line) # and send this one
+					except AttributeError:
+						print("oh sad, I can't send messages to voice channel text channels.")
 				break
 			elif len(line) > 0:
 				if line.startswith('/') and line.endswith('/'):
@@ -138,5 +142,22 @@ async def on_message(message):
 	for key, reaction in REACTIONS.items():
 		if re.search(key, content, re.IGNORECASE):
 			await message.add_reaction(reaction)
+
+	for song, triggers in SONGS.items(): # if someone says "beans", play the beans song
+		for trigger in triggers:
+			if client.ready_to_play and re.search(fr"\b{trigger}", content, re.IGNORECASE):
+				if message.author.voice is not None and message.author.voice.channel is not None:
+					voice_channel = message.author.voice.channel
+					if voice_channel is not None:
+						client.ready_to_play = False
+						print("zayo sonda {}.mp3".format(song))
+						voice_client = await voice_channel.connect()
+						voice_client.play(discord.FFmpegPCMAudio('./res/{}.mp3'.format(song)))
+						while voice_client.is_playing():
+							await asyncio.sleep(1)
+						await voice_client.disconnect()
+						print("lewo sonda {}.mp3".format(song))
+						client.ready_to_play = True
+						break
 
 client.run(os.getenv('TOKEN'))
